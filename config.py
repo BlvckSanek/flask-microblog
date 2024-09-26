9acc6caf1234bef79860682f8f332d86dd03a8fd
Emmanuel Arkoh-Nelson <arkohnelsonemmanuel@gmail.com>
2024-09-26 17:31:00 +0000

    feat: added another language(Spanish) translation to the platform.

--- config.py
+++ config.py
@@ -10,5 +10,7 @@ class Config:
     MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS") is not None
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    ADMINS = ["your-email@example.com", "neslenimma@gmail.com"]
+    ADMINS = ["your-email@example.com"]
     POSTS_PER_PAGE = 25
+    LANGUAGES = ["en", "es"]
+    
